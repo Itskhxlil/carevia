ea37c19db7bba67a81bb7e5defae43baba10d071
Bouaouk Aymen <aymenbouaouk@gmail.com>
2026-04-30 23:32:54 +0100

    restructure project

---
+++ frontend/src/pages/PatientsPage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PatientList from "../components/PatientList.jsx";
+import PageShell from "./PageShell.jsx";
+
+export default function PatientsPage() {
+  return (
+    <PageShell
+      title="Patients"
+      description="Search, sort, and export your registry. Add records with the button below."
+    >
+      <div className="flex justify-end">
+        <Link to="/dashboard/patients/new" className="carevia-btn-primary">
+          <span className="material-symbols-outlined text-[18px]">person_add</span>
+          Add patient
+        </Link>
+      </div>
+
+      <PatientList />
+    </PageShell>
+  );
+}
